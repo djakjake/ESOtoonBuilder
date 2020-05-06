cc926e2064d85b5e0e624c8a9a84e9eac0582e00
jacluff1 <jacluff1@asu.edu>
2020-05-06 07:21:00 -0700

    added update.sh

--- update.sh
+++ update.sh
@@ -2,9 +2,10 @@
 # script tested on Ubuntu 18.04.3 LTS using Python 3.6.9
 
 # update pip
+pip install -U pip
 
 # update python packages
-pip install -r -U requirements.txt
+pip install -r requirements.txt
 
 # pull latest stable version of ESOtoonBuilder
 git pull origin master
