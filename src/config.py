d746693deaff6233f1d2aa59669d5fd1d104d651
jacluff1 <jacluff1@asu.edu>
2020-05-11 03:04:00 -0700

    added comments and the scale factor options for Attributes

--- src/config.py
+++ src/config.py
@@ -6,15 +6,23 @@
 
 class CharacterOptions:
 
-    #==================#
-    # attribute points #
-    #==================#
+    #========================================================================#
+    # attribute points                                                       #
+    # NOTE: defaults values can be found in                                  #
+    # src/Character/Attributes/__init__.py under "default attribute options" #
+    #========================================================================#
 
     Attributes = {
+        # number of attribute points in:
         'health': 64,
         'magicka': 0,
         'stamina': 0,
+        # the maximum number of allowed attribute points
         'maxPoints': 64,
+        # scale factor used to calculate base stats from attribute points:
+        'healthFactor': 122,
+        'magickaFactor': 111,
+        'staminaFactor': 111,
     }
 
     #=================#
